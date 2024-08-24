aa8b7a16b41aef8e8c9efcf5514ab8190eebb1bf
Arghay013 <arghay.cse8.bu@gmail.com>
2024-08-24 19:16:14 +0600

    commited

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>world tour React</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
